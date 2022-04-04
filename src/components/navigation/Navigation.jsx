b1329bf2f888e47eb985b4555d306cab4f4e9ebd
renataferrando <renataferrando@hotmail.com>
2022-04-04 15:13:42 -0300

    bla

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -7,10 +7,10 @@ const Navigation = ({isOpen, onClose, className, comicsOpen, seriesOpen, width, 
     const navigate = useNavigate()
     return (
         <Drawer className={className} isOpen={isOpen} onClose={onClose} position="left" width={width} closeBtn={true} positionCloseRight>
-           
             {comicsOpen && 
             (<div>
                 <h4>COMICS</h4>
+                <input type="text" />
                 {
                 comics.map(({title, id}) => (
                     <li className='list-items' onClick={()=> navigate(`/comics/${id}`) & onClose()} key={id} >{[title]}</li>
